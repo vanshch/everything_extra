d8f63c7fcc819c55cabfebf11e86ce9c8d8890bd
carstairs <83586800+vanshch@users.noreply.github.com>
2023-11-05 11:19:51 +0530

    Update snippets.cpp

--- snippets.cpp
+++ snippets.cpp
@@ -47,3 +47,69 @@ void opposite(ship& ob, shipInfo info) {
 			ob.Greed_sail(Direction::SOUTH_EAST, 1);
 		}
 	}
+int wheremove(shipInfo info, ship& ob) {
+	int* p = minDist(ob, info);
+	int x = *(p + 1);
+	Greed::coords taret, mine;
+	if (x)
+		taret = info.getCurrentRearTile();
+	else
+		taret = info.getCurrentTile();
+
+	x = *p;
+	if (x)
+		mine = info.getCurrentRearTile();
+	else
+		mine = info.getCurrentTile();
+
+	if (mine.r > taret.r) {
+		if (info.getShipDirection() == Direction::SOUTH || info.getShipDirection() == Direction::SOUTH_EAST || info.getShipDirection() == Direction::SOUTH_WEST) {
+			same(ob, info);
+		}
+		else if (info.getShipDirection() == Direction::EAST || info.getShipDirection() == Direction::WEST) {
+			opposite(ob, info);
+		}
+		else {
+			opposite(ob, info);
+		}
+	}
+	else if (mine.r < taret.r) {
+		if (info.getShipDirection() == Direction::SOUTH || info.getShipDirection() == Direction::SOUTH_EAST || info.getShipDirection() == Direction::SOUTH_WEST) {
+			opposite(ob, info);
+		}
+		else if (info.getShipDirection() == Direction::EAST || info.getShipDirection() == Direction::WEST) {
+			opposite(ob, info);
+		}
+		else {
+			same(ob, info);
+		}
+	}
+	else if (mine.c < taret.r) {
+		if (info.getShipDirection() == Direction::WEST || info.getShipDirection() == Direction::NORTH_WEST || info.getShipDirection() == Direction::SOUTH_WEST) {
+			same(ob, info);
+		}
+		else if (info.getShipDirection() == Direction::NORTH || info.getShipDirection() == Direction::SOUTH) {
+			opposite(ob, info);
+		}
+		else {
+			opposite(ob, info);
+		}
+	}
+	else if (mine.c > taret.r) {
+		if (info.getShipDirection() == Direction::WEST || info.getShipDirection() == Direction::NORTH_WEST || info.getShipDirection() == Direction::SOUTH_WEST) {
+			opposite(ob, info);
+		}
+		else if (info.getShipDirection() == Direction::NORTH || info.getShipDirection() == Direction::SOUTH) {
+			opposite(ob, info);
+		}
+		else {
+			same(ob, info);
+		}
+	}
+
+	if (info.getShipDirection() == Direction::NA) {
+		int r = info.getCurrentTile().r + 3;
+		int c = info.getCurrentTile().c + 3;
+		ob.Greed_setPath(Greed::coords(r, c));
+	}
+}
